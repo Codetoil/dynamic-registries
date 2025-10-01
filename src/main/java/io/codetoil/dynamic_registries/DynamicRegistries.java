dbfc73f589cfa5729a720b57e112c0a1b7be1eb3
Angelina Michalek <ianthisawesomee@gmail.com>
2025-10-01 11:48:18 -0400

    Transformer works!

--- src/main/java/io/codetoil/dynamic_registries/DynamicRegistries.java
+++ src/main/java/io/codetoil/dynamic_registries/DynamicRegistries.java
@@ -5,6 +5,8 @@ import io.codetoil.dynamic_registries.api.DynamicRegistriesObjectHelper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.FMLServiceProvider;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -23,5 +25,6 @@ public class DynamicRegistries {
     public DynamicRegistries(FMLJavaModLoadingContext context) {
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
         context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+
     }
 }
