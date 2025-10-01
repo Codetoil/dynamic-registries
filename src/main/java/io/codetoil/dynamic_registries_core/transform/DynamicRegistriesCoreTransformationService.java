9ef1ccbcbee40914625dd136de6b89e64b466a4d
Angelina Michalek <ianthisawesomee@gmail.com>
2025-10-01 10:56:47 -0400

    Initial Commit

---
+++ src/main/java/io/codetoil/dynamic_registries_core/transform/DynamicRegistriesCoreTransformationService.java
@@ -0,0 +1,32 @@
+package io.codetoil.dynamic_registries_core.transform;
+
+import cpw.mods.modlauncher.api.IEnvironment;
+import cpw.mods.modlauncher.api.ITransformationService;
+import cpw.mods.modlauncher.api.ITransformer;
+import cpw.mods.modlauncher.api.IncompatibleEnvironmentException;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Set;
+
+public class DynamicRegistriesCoreTransformationService implements ITransformationService {
+    @Override
+    public @NotNull String name() {
+        return "Dynamic Registries Core Transformation Service";
+    }
+
+    @Override
+    public void initialize(IEnvironment environment) {
+
+    }
+
+    @Override
+    public void onLoad(IEnvironment env, Set<String> otherServices) throws IncompatibleEnvironmentException {
+
+    }
+
+    @Override
+    public @NotNull List<ITransformer> transformers() {
+        return List.of();
+    }
+}
